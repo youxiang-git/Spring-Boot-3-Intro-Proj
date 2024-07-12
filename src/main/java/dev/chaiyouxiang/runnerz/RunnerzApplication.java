5ecce0afc541cb78c4bd656ccb347437399ec516
youxiang-git <chaiyouxiang@gmail.com>
2024-07-12 23:46:20 +0800

    added springboot boilerplate

---
+++ src/main/java/dev/chaiyouxiang/runnerz/RunnerzApplication.java
@@ -0,0 +1,13 @@
+package dev.chaiyouxiang.runnerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnerzApplication.class, args);
+	}
+
+}
